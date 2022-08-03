d73ef7da0d9279021ca895ee378e8e036b3bdb66
Amellbot <109649155+Amellbot@users.noreply.github.com>
2022-08-03 23:08:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 let fs = require('fs')                    //CREATOR = GEMPY , DEVBOT
 let chalk = require('chalk')
 global.owner = [
-  ['6287898307350'],
-  ['66287898307350'],
-  ['6287898307350', 'gempy', true]  
+  ['15064043432'],
+  ['15064043432'],
+  ['15064043432', 'Amel', true]  
   
 ] 
-global.ownername = [['Gempy']]
-global.namabot = [['PENTABOT']]
-global.ownerinsta = [['@gmpyyy']]
+global.ownername = [['Amelia Pm']]
+global.namabot = [['Lissa Creator']]
+global.ownerinsta = [['@Lissa']]
 global.mods = [] 
 global.prems = [] 
 
@@ -43,8 +43,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'PENTABOT'
-  var sticker_author = 'By GEMPY'
+  var sticker_name = 'Lissa Dan Amelia'
+  var sticker_author = 'By Amelia'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -59,9 +59,9 @@ fs.watchFile(file_exif, () => {
 })
 
 // Sticker WM
-global.packname = sticker_name
-global.author = sticker_author
-global.wm = 'Created by Gempy'
+global.packname = Amelia 💗
+global.author = Lissa
+global.wm = 'Created by Amelia & Lissa'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
